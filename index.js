6c9b75df24033686f9aa158f3c76ecd6b52d5ca7
Leissan Khan <leissan.khan@gmail.com>
2022-02-03 21:46:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1=31;
+const num2=2;
+let multiply=num1*num2;
 
-// Write your code here
+let random=Math.ceil(Math.random()*10);
+
+
+const num3=16;
+const num4=12;
+let mod=num3%num4;
+
+let max=Math.max(1,2,3,5,7,20);
